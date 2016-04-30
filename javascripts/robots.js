375d20dfe6ac127f31ea9e9b17f08cd6afd02faa
MATT BRUTON <the.bruton@gmail.com>
2016-04-30 13:51:07 -0500

    created base types for robots

--- javascripts/robots.js
+++ javascripts/robots.js
@@ -8,12 +8,59 @@ Robot.Player = function(name) {
   this.weapon = null;
   this.name = name || "Malfunctioning Scrapbot";
   this.health = Math.floor(Math.random() * 15 + 60);
-  this.shield = null;
-  this.dmgInc = null;
-  this.evasion = null;
+  this.shield = 0;
+  this.dmgInc = 0;
+  this.evasion = 0;
 };
 
-let TestBot = new Robot.Player();
+  ///////////////////////
+ //        Types      //
+///////////////////////
+
+/* Drones serve the "Agility" type role in this version of Robot Battledome. Slightly lower base health, but heightened
+   ability to avoid opponent attacks. They will also have the ability to do more damage as the rounds go on, exploiting
+   flaws in their opponent's frame. */
+
+Robot.Drone = function() {
+  this.health -= 10;
+  this.type = "Drone";
+  this.evasion += 10;
+};
+
+Robot.Drone.prototype = new Robot.Player();
+
+/* Tankbots have slightly higher health than the other two types. No Bonus to evasion since they have heavier and 
+   sturdier frames than other types. */
+
+Robot.Tankbot = function() {
+  this.health += 10;
+  this.type = "Tankbot";
+};
+
+Robot.Tankbot.prototype = new Robot.Player();
+
+/* Psybots will have stronger shields, take more damage as rounds increase due to their more fragile frames.
+   Slight bonus to evading attacks based on their ability to calculate opponents next move. */
+
+Robot.Psybot = function() {
+  this.health -= 5;
+  this.type = "Psybot";
+  this.evasion += 5;
+};
+
+Robot.Psybot.prototype = new Robot.Player();
+
+
+  ///////////////////////
+ //   Drone Models    //
+///////////////////////
+
+
+
+
+
+
+let TestBot = new Robot.Tankbot();
 
 
 let testLink = () => console.log(TestBot);
