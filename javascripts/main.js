86475b0cadff1232e315cafc3f46571d33243cbe
MATT BRUTON <the.bruton@gmail.com>
2016-05-02 18:15:23 -0500

    hiding dom elements, have switch statement working correctly for P1

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,51 +5,62 @@ let weapons = require('./weapons.js');
 let mods = require('./mods.js');
 
 $(document).ready(function() {
- $("#game-container").click(function() {
+ $("#title-screen").show();
+
+ $("#title-screen").click(function(e) {
   $("#title-screen").hide();
+  $("#playerOne_charSelect").show();
+  $("#playerOne_name_select").show();
  });
 
+ $(".card__link").click(function(e) {
+    var nextCard = $(this).attr("next");
+    console.log("next", nextCard );
+    var moveAlong = false;
 
-//  $(".card__link").click(function(e) {
-//     var nextCard = $(this).attr("next");
-//     console.log("next", nextCard );
-//     var moveAlong = false;
 
-//     switch (nextCard) {
-//       case "card--class":
-//         moveAlong = ($("#player-name").val() !== "");
-//         console.log("cardClass", moveAlong );
-//         break;
-//       case "card--weapon":
-//         moveAlong = ($("#player-name").val() !== "");
-//         PlayerOne.setClass(pickClassId);
-//         console.log("cardWeapon", moveAlong );
-//         break;
-//       case "card--battleground":
-//         moveAlong = ($("#player-name").val() !== "");
-//         break;
-//     }
+    switch (nextCard) {
+      case "card--class":
+        moveAlong = ($("#player-name").val() !== "");
+        console.log("cardClass", moveAlong );
+        break;
+      case "card--typeP1":
+        moveAlong = ($("#player-name").val() !== "");
+        console.log("cardWeapon", moveAlong );
+        break;
+      case "card--modelP1":
+        moveAlong = ($("#player-name").val() !== "");
+        break;
+      case "card--weaponP1":
+        moveAlong = ($("#player-name").val() !== "");
+        break;
+      case "card--modifyP1":
+        moveAlong = ($("#player-name").val() !== "");
+        break;
+      case "card--confirmP1":
+        moveAlong = ($("#player-name").val() !== "");
+        break;
+    }
 
-//     if (moveAlong) {
-//       $(".card").hide();
-//       $("." + nextCard).show();
-//     }
-//   });
+    if (moveAlong) {
+      $(".card").hide();
+      $("." + nextCard).show();
+    }
+  });
 
-//   /*
-//     When the back button clicked, move back a view
-//    */
-//   $(".card__back").click(function(e) {
-//     var previousCard = $(this).attr("previous");
-//     $(".card").hide();
-//     $("." + previousCard).show();
-//   });
+  /*
+    When the back button clicked, move back a view
+   */
+  $(".card__back").click(function(e) {
+    var previousCard = $(this).attr("previous");
+    $(".card").hide();
+    $("." + previousCard).show();
+  });
 
 // });
 
-
- 
 });
+
 
 
 robots.testLink();
