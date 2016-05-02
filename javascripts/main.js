0c338d72d3dbf661b69d57397d47db8a80b7055e
MATT BRUTON <the.bruton@gmail.com>
2016-05-01 19:34:42 -0500

    added placeholder buttons for prev and next card

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,6 +8,47 @@ $(document).ready(function() {
  $("#game-container").click(function() {
   $("#title-screen").hide();
  });
+
+
+//  $(".card__link").click(function(e) {
+//     var nextCard = $(this).attr("next");
+//     console.log("next", nextCard );
+//     var moveAlong = false;
+
+//     switch (nextCard) {
+//       case "card--class":
+//         moveAlong = ($("#player-name").val() !== "");
+//         console.log("cardClass", moveAlong );
+//         break;
+//       case "card--weapon":
+//         moveAlong = ($("#player-name").val() !== "");
+//         PlayerOne.setClass(pickClassId);
+//         console.log("cardWeapon", moveAlong );
+//         break;
+//       case "card--battleground":
+//         moveAlong = ($("#player-name").val() !== "");
+//         break;
+//     }
+
+//     if (moveAlong) {
+//       $(".card").hide();
+//       $("." + nextCard).show();
+//     }
+//   });
+
+//   /*
+//     When the back button clicked, move back a view
+//    */
+//   $(".card__back").click(function(e) {
+//     var previousCard = $(this).attr("previous");
+//     $(".card").hide();
+//     $("." + previousCard).show();
+//   });
+
+// });
+
+
+ 
 });
 
 
